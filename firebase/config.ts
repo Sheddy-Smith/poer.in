b075495ac43930372f0a6f6e1e68d3901c581ad5
Sheddy-Smith <sheddysmith822@gmail.com>
2025-10-21 21:31:40 +0530

    Refactor Firebase config and initialization

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  "projectId": "studio-7580518911-7fc89",
-  "appId": "1:658611979785:web:11414ef5b6f7cbbe8d9ad8",
-  "apiKey": "AIzaSyD2DjHJH7rdStsKHknTwM8HY1-hZRr2vVo",
-  "authDomain": "studio-7580518911-7fc89.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "658611979785"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2DjHJH7rdStsKHknTwM8HY1-hZRr2vVo",
+  authDomain: "studio-7580518911-7fc89.firebaseapp.com",
+  projectId: "studio-7580518911-7fc89",
+  storageBucket: "studio-7580518911-7fc89.firebasestorage.app",
+  messagingSenderId: "658611979785",
+  appId: "1:658611979785:web:11414ef5b6f7cbbe8d9ad8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
